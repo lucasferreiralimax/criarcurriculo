5990d4bc866a3569294e13609ae70189571feb02
Lucas Lima <lucaslimay@gmail.com>
2018-08-15 05:52:05 -0300

    ajuste na pagina 404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,9 @@
 <template lang="pug">
-section.text-center.animate_intro
+section.mark.text-center.animate_intro
   h1 {{ $t('text.notfound_t1') }}
   p {{ $t('text.notfound_t2') }}
-  br
-  h2
-    | {{ $t('text.notfound_t3') }}
-    router-link.logo(to="/" tag="h3" v-scroll-to="'#start_app'") {{ $t('text.notfound_click') }}
+  h3 {{ $t('text.notfound_t3') }}
+  router-link(to="/" tag="a" v-scroll-to="'#start_app'") {{ $t('text.notfound_click') }}
 </template>
 
 <script>
