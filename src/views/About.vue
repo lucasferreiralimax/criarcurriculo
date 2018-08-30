cc73af5d4e93d5e76050561af98b91a6c28f70ad
Lucas Lima <lucaslimay@gmail.com>
2018-08-30 05:45:04 -0300

    adicionando novas traduções :walking:

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,13 @@
 <template lang="pug">
   section.mark.text-center.animate_intro
-    h1 Informações
-    p Esse web aplicativo é uma ação social e auxiliará você a criar um curriculo profissional, elegante e objetivo, de uma forma totalmente gratuita!
-    p Espera-se que esse aplicativo, ajude você de todas as maneiras portáteis possíveis para geração de seu novo curriculo profissional.
-    p Criado por um rapaz simples que mesmo com as dificuldades do seu cotidiano, focou e dedicou seus estudos para elaborar esse projeto.
-    p Se você deseja ajudar de alguma forma existe esse
-      a(href='https://github.com/lucaslimax/curriculogratis' target='_blank' rel="noopener")  repositório no github
-      | ou no
-      a(href='https://github.com/lucaslimax/curriculogratis' target='_blank' rel="noopener")  repositório no gitlab
-      | Ou ententre em contato lucaslimay@gmail.com
+    h1 {{ $t('nav.about') }}
+    p {{ $t('view.about.text1') }}
+    p {{ $t('view.about.text2') }}
+    p {{ $t('view.about.text3') }}
+      a(href='https://github.com/lucaslimax/curriculogratis' target='_blank' rel="noopener")  {{ $t('view.about.text4') }}
+      | {{ $t('view.about.contact') }}
     br
-    p Atenciosamente, Lucas Lima.
+    h2 {{ $t('view.about.thanks') }}
 </template>
 
 <script>
