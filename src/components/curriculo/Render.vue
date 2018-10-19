1f60f5b9b8ab02275f2eeed22cb96ae7078b237b
Lucas Lima <lucaslimay@gmail.com>
2018-10-19 14:08:34 -0300

    adicionando rotação na foto :+1:

--- src/components/curriculo/Render.vue
+++ src/components/curriculo/Render.vue
@@ -3,7 +3,10 @@
     .view(:class="{ 'viewless' : viewless, 'viewblocked' : viewblocked }")
       label.figure(for="GET-photo" v-if="user.photo")
         figure
-          img(:src="user.photo" :alt="user.name" width="auto")
+          img#photo(:src="user.photo" :alt="user.name" width="auto")
+        button.btn.small(type="button" @click="rotateFigure")
+          svg(xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 481.95 481.95")
+            path(d="M331.5 114.75L216.75 0v79.05C117.3 91.8 38.25 175.95 38.25 280.5s79.05 188.7 178.5 201.45v-51C145.35 418.2 89.25 357 89.25 280.5s56.1-137.7 127.5-150.45v99.45L331.5 114.75zM443.7 255c-5.101-35.7-17.851-68.85-40.8-99.45l-35.7 35.7c12.75 20.4 22.95 40.8 25.5 63.75h51zM267.75 430.95v51c35.7-5.101 68.85-17.851 99.45-40.8l-35.7-35.7c-20.4 12.75-40.8 22.95-63.75 25.5zm99.45-61.2l35.7 35.7c22.949-30.601 38.25-63.75 40.8-99.45h-51c-2.55 22.95-12.75 43.35-25.5 63.75z")
       .info
         label(for="GET-name")
           h1 {{ user.name }}
@@ -60,6 +63,25 @@ export default {
     }
   },
   methods: {
+    rotateFigure () {
+      let photo = document.querySelector('#photo'),
+          transform = photo.style.transform
+
+      switch (transform) {
+        case 'rotate(90deg)':
+          photo.style.transform = "rotate(180deg)"
+          break;
+        case 'rotate(180deg)':
+          photo.style.transform = "rotate(270deg)"
+          break;
+        case 'rotate(270deg)':
+          photo.style.transform = ""
+          break;
+        default:
+          photo.style.transform = "rotate(90deg)"
+          break;
+      }
+    },
     viewMore () {
       this.viewless = !this.viewless
     },
