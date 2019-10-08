63595687a6e1b4c5a9e31f8b94ecbc2202076ad2
lucasferreiralimax <lucasferreiralimax@gmail.com>
2019-10-08 20:43:04 -0300

    refactory in resize textarea

--- src/mixins/mixinResizeTextArea.js
+++ src/mixins/mixinResizeTextArea.js
@@ -1,14 +1,9 @@
 export const mixinResizeTextArea = {
   methods: {
-    resizeTextArea (input, key) {
+    resizeTextArea (input) {
       let $ref = this.$refs[input]
-      if(key >= 0) {
-        $ref[key].style.cssText = 'height:auto; padding:0';
-        $ref[key].style.cssText = 'height:' + ($ref[key].scrollHeight + 2) + 'px';
-      } else {
-        $ref.style.cssText = 'height:auto; padding:0';
-        $ref.style.cssText = 'height:' + ($ref.scrollHeight + 2) + 'px';
-      }
+      this.$refs[input].style.cssText = 'height:auto; padding:0';
+      this.$refs[input].style.cssText = 'height:' + ($ref.scrollHeight + 2) + 'px';
     }
   }
 }
