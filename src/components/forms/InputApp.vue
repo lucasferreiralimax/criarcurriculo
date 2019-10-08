985efcca8ef4044b3b63f45a410ee4a0e8162b5c
lucasferreiralimax <lucasferreiralimax@gmail.com>
2019-10-08 18:01:54 -0300

    new components InputApp and InputCrud

---
+++ src/components/forms/InputApp.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  label(:for="`GET-${name}`" v-bind:class="{ error: !user[value] && errors ? errors.length : false }")
+    | {{ $t(`form.${translate}`) }}
+    input(:id="`GET-${name}`" :name="name" :type="type" :placeholder="$t(`form.${translate}_place`)" :value="user[value]" @input="updateVuex(vuex, $event)")
+    p.error-msg(v-if="!user[value] && errors ? errors.length : false") {{ $t(`form.errors.${translate}`) }}
+</template>
+
+<script>
+import { mixinUpdateStore } from '../../mixins/mixinUpdateStore.js'
+
+export default {
+  name: 'input-app',
+  props: ['name', 'value', 'type', 'vuex', 'translate', 'errors'],
+  mixins: [mixinUpdateStore]
+}
+</script>
+
+<style lang="stylus">
+</style>
---
+++ src/components/forms/InputCrud.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  div
+    label.input__contato(:for="`GET-${name}-` + key" v-for="(email, key, index) in user[value]" v-bind:class="{ error: !user[value][key] && errors ? errors.length : false }")
+      .right
+        button.btn.delete.bullet.small.left(type="button" @click="removeInput(value, key)" :aria-label="$t('aria-label.email_remove')")
+          i -
+        button.btn.plus.bullet.small.left(type="button" @click="newInput(value, vuex)" :aria-label="$t('aria-label.email_add_new')")
+          i +
+      | {{ $t(`form.${translate}`) }}:
+      input(:id="`GET-${name}-` + key" :name="`${name}-` + key" :type="type" :placeholder="$t(`form.${translate}_place`)" @input="updateVuex(vuex, $event)" v-model="user[value][key]")
+      p.error-msg(v-if="!user[value][key] && errors ? errors.length : false") {{ $t(`form.errors.${translate}`) }}
+</template>
+
+<script>
+import { mixinUpdateStore } from '../../mixins/mixinUpdateStore.js'
+
+export default {
+  name: 'input-crud',
+  props: ['name', 'value', 'type', 'vuex', 'translate', 'errors'],
+  mixins: [mixinUpdateStore]
+}
+</script>
+
+<style lang="stylus">
+</style>
