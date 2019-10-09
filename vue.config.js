71eb3c4e4a3461d9e945cbdf8e2a1bb07559b76b
lucasferreiralimax <lucasferreiralimax@gmail.com>
2019-10-09 12:02:54 -0300

    add mixins responsive stylus global

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,16 @@
+var path = require('path');
+
 module.exports = {
   pwa: {
     name: 'Curriculo Grátis',
     themeColor: '#4A4A4A'
+  },
+  css: {
+    loaderOptions: {
+      stylus: {
+        import: [path.resolve(__dirname, "src/stylus/mixins/_responsive")]
+      }
+    }
   },
   productionSourceMap: false
 }
