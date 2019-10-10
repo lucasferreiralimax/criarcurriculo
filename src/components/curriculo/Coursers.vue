1d5b12f42a2e1a981042573cbfe8c9c47a092905
lucasferreiralimax <lucasferreiralimax@gmail.com>
2019-10-10 01:53:20 -0300

    refactory with BoxCurriculo

--- src/components/curriculo/Coursers.vue
+++ src/components/curriculo/Coursers.vue
@@ -1,47 +1,32 @@
 <template lang="pug">
-  .box.coursers
-    button.btn.plus.large(type="button" v-if="user.coursers.length == 0" @click='newComponent()' :aria-label="$t('aria-label.course_add')")
+.coursers
+  box-curriculo(v-if="user.coursers.length !== 0" :id="'course-' + key" :key="key" title="true" type="coursers" :value="{ id: key, parent: 'coursers', child: 'formation'}" translate="cademy" v-for="(course, key, index) in user.coursers")
+    template(#action)
+      .right.ml-1
+        button.btn.delete.bullet.small.left(type="button" @click="removeComponent('coursers', key)" :aria-label="$t('aria-label.course_remove')")
+          i -
+    label.course_now(:for="'GET-coursenow-' + key" v-bind:class="{ 'display-n-print': !course.formation.now }" @keyup.enter='course.formation.now = !course.formation.now')
+      input(:id="'GET-coursenow-' + key" name="'now' + key" type="checkbox" v-model="course.formation.now" @click="updateVuex('updateCourserCheck', $event, key)")
+      | {{ $t('form.cademy_now')}}
+    input-app(:id="key" :name="'school-' + key" :value="{ parent: 'coursers', child: 'formation', content: 'school' }" type="text" translate="school" vuex="updateCourser" :errors="errors")
+    input-app(:id="key" :name="'course-' + key" :value="{ parent: 'coursers', child: 'formation', content: 'name' }" type="text" translate="course" vuex="updateCourser" :errors="errors")
+    label(:for="'GET-coursedata__start-' + key")
+      p {{ $t('form.start')}}:
+      input(:id="'GET-coursedata__start-' + key" name="coursedata-start" type="month" v-model="course.formation.data_start" @input="updateVuex('updateCourser', $event)")
+    label(:for="'GET-coursedata__finish-' + key")
+      p {{ $t('form.finish')}}:
+      input(:id="'GET-coursedata__finish-' + key" name="coursedata-finish" type="month" v-model="course.formation.data_end" @input="updateVuex('updateCourser', $event)")
+    textarea-app(:id="key" :name="'courseabout-' + key" :value="{ parent: 'coursers', child: 'formation'}" translate="about_course" vuex="updateCourser")
+  box-curriculo
+    button.btn.plus.large(type="button" @click="newComponent('coursers')" :aria-label="$t('aria-label.course_add')")
       span +
-      | {{ $t('form.cademy')}}
-    fieldset.course(:id="'course-' + course.id" v-for="(course, key, index) in user.coursers" :key="key")
-      legend
-        svg.icon(viewBox="0 -35 512 512" width="20" xmlns="http://www.w3.org/2000/svg")
-          path(d="M512 135.5c0-13.242-8.941-25.305-24.531-33.102L302.074 9.703C289.56 3.445 273.195 0 255.996 0c-17.203 0-33.57 3.445-46.086 9.703L24.531 102.398C8.941 110.195 0 122.262 0 135.504s8.941 25.305 24.531 33.098l40.383 20.191V293.98c0 29.883 20.934 63.758 47.66 77.125l97.348 48.668c12.922 6.461 29.5 9.692 46.082 9.692s33.164-3.23 46.086-9.692l97.332-48.668c26.726-13.367 47.664-47.242 47.664-77.125V188.797l34.91-17.457v254.707c0 8.285 6.715 15 15 15s15-6.715 15-15V135.57c0-.023.004-.047.004-.07zm-481.965 0c.34-.805 2.25-3.441 7.914-6.273l185.38-92.688C231.636 32.383 243.542 30 255.995 30c12.45 0 24.356 2.383 32.664 6.535L456.59 120.5H312.328c-7.515-20.387-29.922-35.215-56.332-35.215-32.539 0-59.02 22.528-59.02 50.215 0 27.691 26.477 50.219 59.02 50.219 26.41 0 48.82-14.832 56.332-35.219h144.27l-167.926 83.96c-8.313 4.157-20.215 6.54-32.664 6.54h-.004c-12.45 0-24.356-2.383-32.664-6.535L37.95 141.77c-5.665-2.833-7.575-5.465-7.915-6.27zm254.985 0c0 10.96-13.29 20.219-29.024 20.219-15.726 0-29.02-9.258-29.02-20.219 0-10.957 13.294-20.215 29.02-20.215 15.734 0 29.024 9.258 29.024 20.215zm132.066 158.48c0 18.512-14.524 42.012-31.082 50.293l-97.332 48.668c-17.402 8.7-47.934 8.7-65.332 0l-97.348-48.668c-16.555-8.28-31.082-31.78-31.082-50.293v-90.187l115.012 57.504C222.437 267.555 238.8 271 256.004 271s33.566-3.45 46.082-9.707l115-57.5zm0 0")
-        span {{ $t('form.cademy')}} {{ course.formation.name }}
-        .right.ml-1
-          button.btn.delete.bullet.small.left(type="button" @click='removeComponent(key)' :aria-label="$t('aria-label.course_remove')")
-            i -
-          button.btn.plus.bullet.small.left(type="button" @click='newComponent(key)' v-scroll-to="'#course-' + course.id_sibiling" :aria-label="$t('aria-label.course_add_new')")
-            i +
-      // Curso atual?
-      // The Course Now?
-      label.course_now(:for="'GET-coursenow-' + course.id" v-bind:class="{ 'display-n-print': !course.formation.now }" @keyup.enter='course.formation.now = !course.formation.now')
-        input(:id="'GET-coursenow-' + course.id" name="'now' + course.id" type="checkbox" v-model="course.formation.now" @click="updateVuex('updateCourserCheck', $event, course.id)")
-        | {{ $t('form.cademy_now')}}
-
-      // Escola
-      // The School
-      input-app(:id="course.id" :name="'school-' + course.id" :value="{ parent: 'coursers', child: 'formation', content: 'school' }" type="text" translate="school" vuex="updateCourser" :errors="errors")
-
-      // Curso
-      // The Course
-      input-app(:id="course.id" :name="'course-' + course.id" :value="{ parent: 'coursers', child: 'formation', content: 'name' }" type="text" translate="course" vuex="updateCourser" :errors="errors")
-
-      // Data do curso
-      // The Course data
-      label(:for="'GET-coursedata__start-' + course.id")
-        p {{ $t('form.start')}}:
-        input(:id="'GET-coursedata__start-' + course.id" name="coursedata-start" type="month" v-model="course.formation.data_start" @input="updateVuex('updateCourser', $event)")
-      label(:for="'GET-coursedata__finish-' + course.id")
-        p {{ $t('form.finish')}}:
-        input(:id="'GET-coursedata__finish-' + course.id" name="coursedata-finish" type="month" v-model="course.formation.data_end" @input="updateVuex('updateCourser', $event)")
-      // Sobre o curso
-      // The Course About
-      textarea-app(:id="course.id" :name="'courseabout-' + course.id" :value="{ parent: 'coursers', child: 'formation'}" translate="about_course" vuex="updateCourser")
+      | {{ $t('form.cademy') }}
 </template>
 
 <script>
 import { mixinUpdateStore } from '@/mixins/mixinUpdateStore.js'
+
+import BoxCurriculo from '@/components/curriculo/BoxCurriculo'
 import InputApp from '@/components/forms/InputApp'
 import TextareaApp from '@/components/forms/TextareaApp'
 
@@ -49,32 +34,6 @@ export default {
   name: 'experiencies',
   props: ['errors'],
   mixins: [mixinUpdateStore],
-  components: { InputApp, TextareaApp },
-  data () {
-    return {
-      coursers_now: false
-    }
-  },
-  methods: {
-    removeComponent (key) {
-      this.$delete(this.user.coursers, key)
-      this.updateStore()
-    },
-    newComponent (key = -1) {
-      this.user.coursers.push({
-        id: ++key,
-        id_sibiling: ++key,
-        name: 'Formação Academica',
-        formation: {
-          name: '',
-          school: '',
-          data_start: '',
-          data_end: '',
-          about: '',
-          now: false
-        }
-      })
-    }
-  }
+  components: { BoxCurriculo, InputApp, TextareaApp }
 }
 </script>
--- src/components/curriculo/Experiencies.vue
+++ src/components/curriculo/Experiencies.vue
@@ -1,47 +1,32 @@
 <template lang="pug">
-  .box.experiences
-    button.btn.plus.large(type="button" v-if="user.exps.length == 0" @click='newComponent()' :aria-label="$t('aria-label.exp_add')")
+.experiences
+  box-curriculo(v-if="user.exps.length !== 0" :id="'exp-' + key" :key="key" title="true" type="exps" :value="{ id: key, parent: 'exps', child: 'experience' }" translate="exp" v-for="(exp, key, index) in user.exps")
+    template(#action)
+      .right.ml-1
+        button.btn.delete.bullet.small.left(type="button" @click="removeComponent('exps', key)" :aria-label="$t('aria-label.exp_remove')")
+          i -
+    label.exps_now(:for="'GET-expsnow-' + key" v-bind:class="{ active: exp.experience.now, 'display-n-print': !exp.experience.now }" @keyup.enter='exp.experience.now = !exp.experience.now')
+      input(:id="'GET-expsnow-' + key" type="checkbox" v:name="'now' + key" v-model="exp.experience.now" @click="updateVuex('updateExpCheck', $event, key)")
+      | {{ $t('form.exp_now')}}
+    input-app(:id="key" :name="'company-' + key" :value="{ parent: 'exps', child: 'experience', content: 'name' }" type="text" translate="company" vuex="updateExp" :errors="errors")
+    input-app(:id="key" :name="'exps-' + key" :value="{ parent: 'exps', child: 'experience', content: 'work' }" type="text" translate="office" vuex="updateExp" :errors="errors")
+    label(:for="'GET-expsdata__start-' + key")
+      p {{ $t('form.start')}}:
+      input(:id="'GET-expsdata__start-' + key" name="expsdata-start" type="month" v-model="exp.experience.data_start" @input="updateVuex('updateExp', $event)")
+    label(:for="'GET-expsdata__finish' + key" v-if="!exp.experience.now")
+      p(v-show="!exp.experience.now") {{ $t('form.finish')}}:
+      input(:id="'GET-expsdata__finish-' + key" name="expsdata-finish" type="month" v-model="exp.experience.data_end" @input="updateVuex('updateExp', $event)")
+    textarea-app(:id="key" :name="'expsabout-' + key" :value="{ parent: 'exps', child: 'experience' }" translate="activities" vuex="updateExp")
+  box-curriculo
+    button.btn.plus.large(type="button" @click="newComponent('exps')" :aria-label="$t('aria-label.exp_add')")
       span +
-      | {{ $t('form.exp')}}
-    fieldset.experience(v-for="(exp, key, index) in user.exps" :id="'exp-' + exp.id" :key="key")
-      legend
-        svg.icon(viewBox="0 -31 512 512" xmlns="http://www.w3.org/2000/svg" width="20")
-          path(d="M497.094 60.004c-.031 0-.063-.004-.094-.004H361V45c0-24.813-20.188-45-45-45H196c-24.813 0-45 20.188-45 45v15H15C6.648 60 0 66.844 0 75v330c0 24.813 20.188 45 45 45h422c24.813 0 45-20.188 45-45V75.316v-.058c-.574-9.852-6.633-15.2-14.906-15.254zM181 45c0-8.27 6.73-15 15-15h120c8.27 0 15 6.73 15 15v15H181zm295.188 45l-46.583 139.742A14.975 14.975 0 0 1 415.38 240H331v-15c0-8.285-6.715-15-15-15H196c-8.285 0-15 6.715-15 15v15H96.621a14.975 14.975 0 0 1-14.226-10.258L35.813 90zM301 240v30h-90v-30zm181 165c0 8.27-6.73 15-15 15H45c-8.27 0-15-6.73-15-15V167.434l23.934 71.796A44.935 44.935 0 0 0 96.62 270H181v15c0 8.285 6.715 15 15 15h120c8.285 0 15-6.715 15-15v-15h84.379a44.935 44.935 0 0 0 42.687-30.77L482 167.434zm0 0")
-        span {{ $t('form.exp')}} {{ exp.experience.work }}
-        .right.ml-1
-          button.btn.delete.bullet.small.left(type="button" @click="removeComponent(key)" :aria-label="$t('aria-label.exp_remove')")
-            i -
-          button.btn.plus.bullet.small.left(type="button" @click="newComponent(key)" v-scroll-to="'#exp-' + exp.id_sibiling" :aria-label="$t('aria-label.exp_add_new')")
-            i +
-      // Experiência atual?
-      // The exps Now?
-      label.exps_now(:for="'GET-expsnow-' + exp.id" v-bind:class="{ active: exp.experience.now, 'display-n-print': !exp.experience.now }" @keyup.enter='exp.experience.now = !exp.experience.now')
-        input(:id="'GET-expsnow-' + exp.id" type="checkbox" v:name="'now' + exp.id" v-model="exp.experience.now" @click="updateVuex('updateExpCheck', $event, exp.id)")
-        | {{ $t('form.exp_now')}}
-
-      // Compania
-      // The Company
-      input-app(:id="exp.id" :name="'company-' + exp.id" :value="{ parent: 'exps', child: 'experience', content: 'name' }" type="text" translate="company" vuex="updateExp" :errors="errors")
-
-      // Experiência
-      // The experience
-      input-app(:id="exp.id" :name="'exps-' + exp.id" :value="{ parent: 'exps', child: 'experience', content: 'work' }" type="text" translate="office" vuex="updateExp" :errors="errors")
-
-      // Data da experiência
-      // The experience data
-      label(:for="'GET-expsdata__start-' + exp.id")
-        p {{ $t('form.start')}}:
-        input(:id="'GET-expsdata__start-' + exp.id" name="expsdata-start" type="month" v-model="exp.experience.data_start" @input="updateVuex('updateExp', $event)")
-      label(:for="'GET-expsdata__finish' + exp.id" v-if="!exp.experience.now")
-        p(v-show="!exp.experience.now") {{ $t('form.finish')}}:
-        input(:id="'GET-expsdata__finish-' + exp.id" name="expsdata-finish" type="month" v-model="exp.experience.data_end" @input="updateVuex('updateExp', $event)")
-      // Sobre a experiência
-      // The exps About
-      textarea-app(:id="exp.id" :name="'expsabout-' + exp.id" :value="{ parent: 'exps', child: 'experience' }" translate="activities" vuex="updateExp")
+      | {{ $t('form.exp') }}
 </template>
 
 <script>
 import { mixinUpdateStore } from '@/mixins/mixinUpdateStore.js'
+
+import BoxCurriculo from '@/components/curriculo/BoxCurriculo'
 import InputApp from '@/components/forms/InputApp'
 import TextareaApp from '@/components/forms/TextareaApp'
 
@@ -49,31 +34,10 @@ export default {
   name: 'experiencies',
   props: ['errors'],
   mixins: [mixinUpdateStore],
-  components: { InputApp, TextareaApp },
+  components: { BoxCurriculo, InputApp, TextareaApp },
   data () {
     return {
       exps_now: false
-    }
-  },
-  methods: {
-    removeComponent (key) {
-      this.$delete(this.user.exps, key)
-      this.updateStore()
-    },
-    newComponent (key = -1) {
-      this.user.exps.push({
-        id: ++key,
-        id_sibiling: ++key,
-        name: this.$i18n.t('form.course'),
-        experience: {
-          name: '',
-          work: '',
-          data_start: '',
-          data_end: '',
-          about: '',
-          now: false
-        }
-      })
     }
   }
 }
