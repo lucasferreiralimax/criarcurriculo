9dedb08c1badb4eef7ab017690f93a57a76bbe3b
lucasferreiralimax <lucasferreiralimax@gmail.com>
2019-10-11 02:25:25 -0300

    implement resize-text-area, remove ref template and mixin resize

--- src/components/forms/TextareaApp.vue
+++ src/components/forms/TextareaApp.vue
@@ -3,27 +3,26 @@
     p {{ (id >= 0) ? $t('form.about') : $t(`form.${name}_me`) }}:
     textarea(v-if="id >= 0"
             :id="`GET-${name}`"
-            :ref="`GET_${name}`"
             :placeholder="$t(`form.${translate}_place`)"
+            v-resize-text-area
             v-model="user[value.parent][id][value.child].about"
-            @click="resizeTextArea(`GET_${name}`)"
-            @input="resizeTextArea(`GET_${name}`), updateVuex(vuex, $event)")
+            @input="updateVuex(vuex, $event)")
     textarea(v-else
             :id="`GET-${name}`"
-            :ref="`GET_${name}`"
             :placeholder="$t(`form.${translate}_place`)"
+            v-resize-text-area
             v-model="user[value]"
-            @click="resizeTextArea(`GET_${name}`)"
-            @input="resizeTextArea(`GET_${name}`), updateVuex(vuex, $event)")
+            @input="updateVuex(vuex, $event)")
 </template>
 
 <script>
 import { mixinUpdateStore } from '@/mixins/mixinUpdateStore.js'
-import { mixinResizeTextArea } from '@/mixins/mixinResizeTextArea.js'
+import { resizeTextArea } from '@/directive/resize.js'
 
 export default {
   name: 'textarea-app',
   props: ['name', 'value', 'id', 'translate', 'vuex'],
-  mixins: [mixinUpdateStore, mixinResizeTextArea]
+  mixins: [mixinUpdateStore],
+  directives: { 'resize-text-area': resizeTextArea }
 }
 </script>
