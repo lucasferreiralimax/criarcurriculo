77e051f30ee114031888c15cfcdc455304aff80e
lucasferreiralimax <lucasferreiralimax@gmail.com>
2019-10-11 02:24:05 -0300

    new directive resize-text-area

---
+++ src/directive/resize.js
@@ -0,0 +1,9 @@
+export const resizeTextArea = {
+  bind (el) { el.addEventListener("click", () => resize(el)) },
+  componentUpdated(el) { resize(el) }
+}
+
+function resize(el) {
+  el.style.cssText = 'height:auto; padding:0'
+  el.style.cssText = 'height:' + (el.scrollHeight + 2) + 'px'
+}
