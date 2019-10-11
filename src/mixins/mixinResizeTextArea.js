18057d37ba6235039d374873ce2bc70525bb6ea1
lucasferreiralimax <lucasferreiralimax@gmail.com>
2019-10-11 02:23:31 -0300

    remove mixin ResizeTextArea

--- src/mixins/mixinResizeTextArea.js
+++
@@ -1,9 +0,0 @@
-export const mixinResizeTextArea = {
-  methods: {
-    resizeTextArea (input) {
-      let $ref = this.$refs[input]
-      $ref.style.cssText = 'height:auto; padding:0';
-      $ref.style.cssText = 'height:' + ($ref.scrollHeight + 2) + 'px';
-    }
-  }
-}
