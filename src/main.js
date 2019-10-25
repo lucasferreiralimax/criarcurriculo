5995d164d262a0397d4e42f1752fb1941557429d
lucasferreiralimax <lucasferreiralimax@gmail.com>
2019-10-25 00:16:26 -0300

    remove v-scroll plugin

--- src/main.js
+++ src/main.js
@@ -7,12 +7,10 @@ import store from './store'
 import './registerServiceWorker'
 
 import VueHead from 'vue-head'
-import VueScrollTo from 'vue-scrollto'
 import HeaderApp from '@/components/base/Header.vue'
 import FooterApp from '@/components/base/Footer.vue'
 
 Vue.use(VueHead)
-Vue.use(VueScrollTo)
 Vue.component('header-app', HeaderApp)
 Vue.component('footer-app', FooterApp)
 
