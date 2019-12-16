f1b52b2536e8b07590b424f0b30b16b5472f5713
lucasferreiralimax <lucasferreiralimax@gmail.com>
2019-12-15 21:49:06 -0300

    new component accounts in Header

---
+++ src/components/Accounts.vue
@@ -0,0 +1,60 @@
+<template lang="pug">
+section.accounts(v-if="accounts.length !== 0")
+  h2 Currículo salvo
+  p(v-for="(account, key) in accounts" @click="updateAccount(key)") {{ account.name }}
+    button(@click="removeAccount(key)" class="btn delete bullet small right" type="button") X
+</template>
+
+<script>
+import { mixinUpdateStore } from '@/mixins/mixinUpdateStore.js'
+
+export default {
+  name: 'accounts',
+  mixins: [mixinUpdateStore],
+  data () {
+    return {
+      accounts: [
+        {
+          name: 'Lucas'
+        },
+        {
+          name: 'Mohamed'
+        }
+      ]
+    }
+  },
+  created () {
+    // console.log(localStorage.getItem(`store-${document.documentElement.lang}`))
+  },
+  methods: {
+    removeAccount ( key) {
+      this.$delete(this.accounts, key)
+      this.updateStore()
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.accounts
+  background transparent
+  padding 1em
+  max-width 300px
+  margin 0 auto
+  h2
+    color #fff
+    font-size 1em
+    margin-bottom 1em
+    text-align center
+  p
+    background #fff
+    line-height 30px
+    text-indent 10px
+    padding 10px
+    margin-bottom 1em
+    border-radius 6px
+    cursor pointer
+    &:last-of-type
+      margin-bottom 0
+
+</style>
--- src/components/base/Header.vue
+++ src/components/base/Header.vue
@@ -4,6 +4,7 @@ header
     router-link(to="/" tag="h1" v-scroll-to="'#curriculo'").logo.text-center {{ $t('view.home.name1')}} {{ $t('view.home.name2')}}
   nav-menu
   pluralization
+  accounts
   theme-app
   scroll-top
 </template>
@@ -11,13 +12,14 @@ header
 <script>
 import NavMenu from '@/components/base/Nav'
 import Pluralization from '@/components/Pluralization'
+import Accounts from '@/components/Accounts'
 import ThemeApp from '@/components/ThemeApp'
 import ScrollTop from '@/components/ScrollTop'
 import { scrollTo } from '@/directive/scroll.js'
 
 export default {
   name: 'header-app',
-  components: { NavMenu, Pluralization, ThemeApp, ScrollTop },
+  components: { NavMenu, Pluralization, Accounts, ThemeApp, ScrollTop },
   directives: { 'scroll-to': scrollTo },
   created () { window.addEventListener('scroll', this.fixedHeader) },
   destroyed () { window.removeEventListener('scroll', this.fixedHeader) },
