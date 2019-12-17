7f0bfff4715d3aacdef95211b9126e8cce57c638
lucasferreiralimax <lucasferreiralimax@gmail.com>
2019-12-17 10:15:56 -0300

    flag in accounts user

--- src/components/Accounts.vue
+++ src/components/Accounts.vue
@@ -1,7 +1,10 @@
 <template lang="pug">
 section.accounts(v-if="accounts.length !== 0")
   h2 {{ $t('view.home.cache')}}
-  p(v-for="(account, key) in accounts" @click="toggleAccount(account)") {{ account.s_name }}
+  p(v-for="(account, key) in accounts" @click="toggleAccount(account)")
+    flag.flag(:type="account.s_lang")
+    | {{ account.s_name }}
+    span {{ account.s_time }}
     button(@click="removeAccount(key, account.s_local)" class="btn delete bullet small right" type="button") X
 </template>
 
@@ -9,12 +12,13 @@ section.accounts(v-if="accounts.length !== 0")
 import { loadLanguageAsync } from '@/i18n'
 import { mixinUpdateStore } from '@/mixins/mixinUpdateStore.js'
 
+import Flag from '@/components/Flag'
+
 export default {
   name: 'accounts',
   mixins: [mixinUpdateStore],
-  created () {
-    this.accountsRender()
-  },
+  components: { Flag },
+  created () { this.accountsRender() },
   methods: {
     toggleAccount (store) {
       this.$store.commit("updateUser", store.s_data)
@@ -22,8 +26,10 @@ export default {
       localStorage.setItem('locale', store.s_lang)
     },
     removeAccount (key, store) {
-      this.$delete(this.accounts, key)
-      localStorage.removeItem(`${store}`)
+      if(window.confirm("Tem certeza?")) {
+        this.$delete(this.accounts, key)
+        localStorage.removeItem(`${store}`)
+      }
     }
   }
 }
@@ -42,11 +48,23 @@ export default {
     background #fff
     line-height 30px
     text-indent 10px
+    text-align left
     padding 10px
     margin-bottom 1em
     border-radius 6px
     cursor pointer
     &:last-of-type
       margin-bottom 0
-
+    .flag
+      transform translate(-5px, 5px)
+    span
+      font-size 11px
+      margin 0 3px
+      opacity .6
+    span,
+    button
+      display inline-block
++htmlDir()
+  .flag
+    transform translate(5px, 5px)
 </style>
---
+++ src/components/Flag.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+.flag
+  img(v-if="type == 'pt-BR'" src="./../assets/flags/br.svg" alt="Brasil" width="30")
+  img(v-if="type == 'ar-SA'" src="./../assets/flags/sa.svg" alt="Arábia" width="30")
+  img(v-if="type == 'en-US'" src="./../assets/flags/us.svg" alt="United States" width="30")
+  img(v-if="type == 'es-ES'" src="./../assets/flags/es.svg" alt="Spain" width="30")
+  img(v-if="type == 'ru-RU'" src="./../assets/flags/ru.svg" alt="Russia" width="30")
+  img(v-if="type == 'in-ID'" src="./../assets/flags/in.svg" alt="India" width="30")
+  img(v-if="type == 'ja-JP'" src="./../assets/flags/jp.svg" alt="Japan" width="30")
+  img(v-if="type == 'fr-FR'" src="./../assets/flags/fr.svg" alt="França" width="30")
+  img(v-if="type == 'tr-TR'" src="./../assets/flags/tr.svg" alt="Turquia" width="30")
+  img(v-if="type == 'ch-ZH'" src="./../assets/flags/ch.svg" alt="China" width="30")
+  img(v-if="type == 'al-DE'" src="./../assets/flags/al.svg" alt="Alemanha" width="30")
+  img(v-if="type == 'it-IT'" src="./../assets/flags/it.svg" alt="Italia" width="30")
+  img(v-if="type == 'gr-GK'" src="./../assets/flags/gr.svg" alt="Grecia" width="30")
+  img(v-if="type == 'tl-TD'" src="./../assets/flags/tl.svg" alt="Tailand" width="30")
+  img(v-if="type == 'nl-HL'" src="./../assets/flags/nh.svg" alt="Holandes" width="30")
+  img(v-if="type == 'nl-NL'" src="./../assets/flags/nl.svg" alt="Noruegues" width="30")
+</template>
+
+<script>
+export default {
+  name: 'flag',
+  props: ['type']
+}
+</script>
+
+<style lang="stylus">
+.flag
+  width 30px
+  height 22px
+  background transparent
+  padding 0
+  margin 0
+  display inline-block
+  position relative
+  img
+    position absolute
+    top 0
+    right 0
+    left 0
+</style>
