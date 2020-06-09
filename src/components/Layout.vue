2d483e61ab5912210e53dc868a05a00b86ca5ffe
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-06-09 20:37:12 -0300

    rename class and refactory

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -4,13 +4,13 @@ section.layout
   .options
     button.btn.m-0(type="button" @click="setLayout()")
       | {{ $t('layout.default') }}
-      .layout-grid.layout-example-1
-    button.btn.m-0(type="button" @click="setLayout('layout_1')")
+      .layout-grid.layout-1
+    button.btn.m-0(type="button" @click="setLayout('layout-circle')")
       | {{ $t('layout.circle') }}
-      .layout-grid.layout-example-2
-    button.btn.m-0(type="button" @click="setLayout('layout_2')")
+      .layout-grid.layout-2
+    button.btn.m-0(type="button" @click="setLayout('layout-mirror')")
       | {{ $t('layout.mirror') }}
-      .layout-grid.layout-example-3
+      .layout-grid.layout-3
 </template>
 
 <script>
@@ -35,53 +35,6 @@ export default {
 </script>
 
 <style lang="stylus">
-.layout_1
-  .figure
-    height 120px
-    width 120px
-    border-radius 100%
-    .btn
-      bottom 0
-      transform translateY(calc(-100% - 15px)) scale(.9)
-  .info.photo
-    padding-left 140px
-.layout_2
-  .info,
-  .exps,
-  .cademy,
-  .hobby,
-  label[for="GET-about"]
-    text-align right
-  .contact-info
-    float right
-  .figure
-    right 3em
-  .info.photo
-    padding-left 0
-    padding-right 140px
-  label
-    &[for^='GET-telephone'],
-    &[for^='GET-email'],
-    &[for^='GET-site']
-      margin-right 0
-      margin-left 1em
-.layout
-  background transparent
-  padding 0 0 1em
-  margin 0 auto
-  h2
-    color #fff
-    font-size 1em
-    margin-bottom 1em
-  .options
-    display grid
-    grid-template-columns repeat(3, 1fr)
-    grid-column-gap 1em
-    grid-row-gap 1em
-  .btn
-    background #fff
-    &:hover
-      background #fff
 .layout-grid
   background repeating-linear-gradient(to top, #999 15%, #999 18%,  transparent  18%, transparent 25%)
   width 100%
@@ -102,32 +55,85 @@ export default {
     position absolute
     right 0
     top 0
-.layout-example-1
+.layout
+  background transparent
+  padding 0 0 1em
+  margin 0 auto
+  h2
+    color #fff
+    font-size 1em
+    margin-bottom 1em
+  .options
+    display grid
+    grid-template-columns repeat(3, 1fr)
+    grid-column-gap 1em
+    grid-row-gap 1em
+  .btn
+    background #fff
+    &:hover
+      background #fff
+.layout-1,
+.layout-2,
+.layout-3
   &:before
     border 3px solid #999
-    outline 3px solid #fff
-.layout-example-2
-  &:before
-    border 3px solid #999
-    border-radius 100%
     box-shadow 0 0 0 6px #fff
+.layout-1
+  &:before
+    outline 3px solid #fff
+.layout-2
+  &:before
+    border-radius 100%
   &:after
     height 30px
-.layout-example-3
+.layout-3
   &:before
-    border 3px solid #999
-    box-shadow 0 0 0 6px #fff
     position absolute
     right 0
   &:after
     top 0
     left 0
     height 20px
+.layout-circle
+  .figure
+    height 120px
+    width 120px
+    border-radius 100%
+    .btn
+      bottom 0
+      transform translateY(calc(-100% - 15px)) scale(.9)
+  .info.photo
+    padding-left 140px
+.layout-mirror
+  .info,
+  .exps,
+  .cademy,
+  .hobby,
+  label[for="GET-about"]
+    text-align right
+  .contact-info
+    float right
+  .figure
+    right 3em
+  .info.photo
+    padding-left 0
+    padding-right 140px
+  label
+    &[for^='GET-telephone'],
+    &[for^='GET-email'],
+    &[for^='GET-site']
+      margin-right 0
+      margin-left 1em
+
 +htmlDir()
-  .layout_1
+  .layout-grid
+    &:after
+      right auto
+      left 0
+  .layout-1
     .info.photo
       padding-right 140px
-  .layout_2
+  .layout-2
     text-align left
     .info,
     .exps,
@@ -150,11 +156,7 @@ export default {
       &[for^='GET-site']
         margin-right 1em !important
         margin-left 0 !important
-  .layout-grid
-    &:after
-      right auto
-      left 0
-  .layout-example-3
+  .layout-3
     &:before
       right auto
       left 0
