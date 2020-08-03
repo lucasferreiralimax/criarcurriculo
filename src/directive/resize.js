98dfa4cd9ebbdc0b2814deb88d06331524fa73ee
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-08-03 04:17:41 -0300

    ajuste no resize textarea

--- src/directive/resize.js
+++ src/directive/resize.js
@@ -5,5 +5,7 @@ export const resizeTextArea = {
 
 function resize(el) {
   el.style.cssText = 'height:auto; padding:0'
-  el.style.cssText = 'height:' + (el.scrollHeight + 2) + 'px'
+  el.style.cssText = 'height:' + (el.scrollHeight < 50 ? 50 : el.scrollHeight) + 'px'
+
+  console.log(el.scrollHeight)
 }
