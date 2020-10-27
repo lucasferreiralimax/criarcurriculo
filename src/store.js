f2b9237bdcf1898cd8e0fd2de7c997bb9bde4228
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-10-27 17:22:38 -0300

    abstract file mutations in store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { user } from './user'
+import { mutations } from './mutations'
 
 Vue.use(Vuex)
 
@@ -12,89 +13,5 @@ export default new Vuex.Store({
     icon_render: true,
     theme_app: 'default'
   },
-  mutations: {
-    updateIconRender (state, value) {
-      state.icon_render = value
-    },
-    updateTheme (state, value) {
-      state.theme_app = value
-    },
-    updateAcccount (state, value) {
-      state.accounts = value
-    },
-    insertAcccount (state, value) {
-      value.s_lang == 'pt-BR' ?
-        state.accounts.unshift(value) :
-        state.accounts.push(value)
-    },
-    updateUser (state, value) {
-      state.user = value
-    },
-    updateName (state, value) {
-      state.user.name = value
-    },
-    updatePhoto (state, value) {
-      state.user.photo = value
-    },
-    updateAge (state, value) {
-      state.user.age = value
-    },
-    updateAddressNumber (state, value) {
-      state.user.addressNumber = value
-    },
-    updateCep (state, value) {
-      state.user.cep = value
-    },
-    updateGenero (state, value) {
-      state.user.genero = value
-    },
-    updateTravel (state, value) {
-      state.user.travel = value
-    },
-    updateAbout (state, value) {
-      state.user.about = value
-    },
-    updateHobbies (state, value) {
-      state.user.hobbies = value
-    },
-    updateTelephone (state, value) {
-      state.user.telephones = value
-    },
-    updateEmail (state, value) {
-      state.user.emails = value
-    },
-    updateSite (state, value) {
-      state.user.sites = value
-    },
-    updateLanguage (state, value) {
-      state.user.languages = value
-    },
-    updateExp (state, value, id) {
-      state.user.exps[id] = value
-    },
-    updateCourser (state, value, id) {
-      state.user.coursers[id] = value
-    },
-    updateExpCheck (state, payload) {
-      state.user.exps[payload.id].experience.now = payload.value
-    },
-    updateCourserCheck (state, payload) {
-      state.user.coursers[payload.id].formation.now = payload.value
-    },
-    updateMaritalStatus (state, value) {
-      state.user.maritalstatus = value
-    },
-    updateEnd (state, value) {
-      state.user.end = value
-    },
-    updateEndLogradouro (state, value) {
-      state.user.end.logradouro = value
-    },
-    updateEndLocalidade (state, value) {
-      state.user.end.localidade = value
-    },
-    updateErrors (state, value) {
-      state.errors = value
-    }
-  }
+  mutations: mutations
 })
