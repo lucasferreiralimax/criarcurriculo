4ba2a2b71e9e6decbe67e44a7e041420e9ad3991
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-10-29 21:06:41 -0300

    Nav test unit itens and menu_show

--- src/components/base/Nav.spec.js
+++ src/components/base/Nav.spec.js
@@ -12,4 +12,14 @@ describe('Nav', () => {
   it('is Nav a vue instance', () => {
     expect(wrapper.isVueInstance()).toBe(true)
   })
+  it('Nav contains itens menu', () => {
+    expect(wrapper.findAll('a.btn').exists()).toBe(true)
+  })
+  it('Nav show menu hidden or disable', () => {
+    expect(wrapper.vm.menu_show).toBe(false)
+  })
+  it('Nav show menu', () => {
+    wrapper.find('.btn.menu').trigger('click')
+    expect(wrapper.vm.menu_show).toBe(true)
+  })
 })
