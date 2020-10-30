876cc49f1f36c113535b9b1974396f2faad00148
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-10-29 21:05:34 -0300

    Footer test unit LogoApp and credits L

--- src/components/base/Footer.spec.js
+++ src/components/base/Footer.spec.js
@@ -1,11 +1,19 @@
 import { shallowMount } from '@vue/test-utils'
 
 import Footer from './Footer.vue'
+import LogoApp from '@/components/base/Logo'
 
 const wrapper = shallowMount(Footer)
+const credits = wrapper.find('.credits.L')
 
 describe('Footer', () => {
   it('is Footer a vue instance', () => {
     expect(wrapper.isVueInstance()).toBe(true)
+  })
+  it('Footer contains LogoApp', () => {
+    expect(wrapper.contains(LogoApp)).toBe(true)
+  })
+  it('Footer contains credits L', () => {
+    expect(credits.exists()).toBe(true)
   })
 })
