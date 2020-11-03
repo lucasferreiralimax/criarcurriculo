197da8c4044eaec24b4efd5aa4a44cd47f7e94a4
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-11-03 18:48:46 -0300

    test unit accounts simple component

---
+++ src/components/Accounts.spec.js
@@ -0,0 +1,27 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+
+import { user } from '@/user'
+import { mutations } from '@/mutations'
+import Vuex from 'vuex'
+import Accounts from './Accounts.vue'
+import draggable from 'vuedraggable'
+
+const localVue = createLocalVue()
+localVue.component(draggable.name, draggable)
+
+localVue.use(Vuex)
+
+let store = new Vuex.Store({
+  state: {
+    user: user
+  },
+  mutations: mutations
+})
+
+const wrapper = shallowMount(Accounts, { store, localVue })
+
+describe('Accounts', () => {
+  it('is Accounts a vue instance', () => {
+    expect(wrapper.isVueInstance()).toBe(true)
+  })
+})
