a1c221289929687ee645c0ffb0981a2a17cd64f9
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-11-05 20:41:31 -0300

    import Carousel test unit

--- src/views/Home.spec.js
+++ src/views/Home.spec.js
@@ -2,10 +2,13 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 
 import Vuex from 'vuex'
 import Home from './Home.vue'
+import { Carousel, CarouselItem } from 'element-ui'
 
 const localVue = createLocalVue()
 
 localVue.use(Vuex)
+localVue.use(Carousel)
+localVue.use(CarouselItem)
 
 let store = new Vuex.Store({
   state: {
