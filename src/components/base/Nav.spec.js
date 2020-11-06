39fc7bb626177b4b5fc31657e011022af1d706e0
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-11-05 21:05:44 -0300

    rename tests with toBeTruthy, toBeFalsy and toContain

--- src/components/base/Nav.spec.js
+++ src/components/base/Nav.spec.js
@@ -13,13 +13,13 @@ describe('Nav', () => {
     expect(wrapper.exists()).toBeTruthy()
   })
   it('Nav contains itens menu', () => {
-    expect(wrapper.findAll('a.btn').exists()).toBe(true)
+    expect(wrapper.findAll('a.btn').exists()).toBeTruthy()
   })
   it('Nav show menu hidden or disable', () => {
     expect(wrapper.vm.menu_show).toBe(false)
   })
   it('Nav show menu', () => {
     wrapper.find('.btn.menu').trigger('click')
-    expect(wrapper.vm.menu_show).toBe(true)
+    expect(wrapper.vm.menu_show).toBeTruthy()
   })
 })
