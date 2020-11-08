1790d3223abf05e89056f9e8b141fb5eb4bda3cb
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-11-08 16:23:37 -0300

    test unit cep

--- src/components/forms/InputCep.spec.js
+++ src/components/forms/InputCep.spec.js
@@ -1,6 +1,7 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 
 import { user } from '@/user'
+import { mutations } from '@/mutations'
 import Vuex from 'vuex'
 import InputCep from './InputCep.vue'
 
@@ -11,13 +12,58 @@ localVue.use(Vuex)
 let store = new Vuex.Store({
   state: {
     user: user
-  }
+  },
+  mutations: mutations
 })
+
+let endTestFull = {
+  bairro: "Jardim Matarazzo",
+  cep: "03814-020",
+  complemento: "",
+  ddd: "11",
+  gia: "1004",
+  ibge: "3550308",
+  localidade: "São Paulo",
+  logradouro: "Rua Manuel de Mattos Godinho",
+  siafi: "7107",
+  uf: "SP"
+}
+
+let endTestClean = {
+  bairro: "",
+  cep: "",
+  complemento: "",
+  ddd: "",
+  gia: "",
+  ibge: "",
+  localidade: "",
+  logradouro: "",
+  siafi: "",
+  uf: ""
+}
 
 const wrapper = shallowMount(InputCep, { localVue, store })
 
 describe('InputCep', () => {
   it('is InputCep a vue instance', () => {
     expect(wrapper.exists()).toBeTruthy()
+  })
+  it('is InputCep test a CEP valid', () => {
+    store.commit('updateEnd', endTestFull)
+    expect(wrapper.vm.user.end.cep).toBe('03814-020')
+    expect(wrapper.vm.user.end.bairro).toBe('Jardim Matarazzo')
+    expect(wrapper.vm.user.end.ddd).toBe('11')
+    expect(wrapper.vm.user.end.localidade).toBe('São Paulo')
+    expect(wrapper.vm.user.end.logradouro).toBe('Rua Manuel de Mattos Godinho')
+    expect(wrapper.vm.user.end.uf).toBe('SP')
+  })
+  it('is InputCep test a CEP empty', () => {
+    store.commit('updateEnd', endTestClean)
+    expect(wrapper.vm.user.end.cep).toBe('')
+    expect(wrapper.vm.user.end.bairro).toBe('')
+    expect(wrapper.vm.user.end.ddd).toBe('')
+    expect(wrapper.vm.user.end.localidade).toBe('')
+    expect(wrapper.vm.user.end.logradouro).toBe('')
+    expect(wrapper.vm.user.end.uf).toBe('')
   })
 })
