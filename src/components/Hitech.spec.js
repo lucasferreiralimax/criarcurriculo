ca64c5e1ebcf367fa3dc5d34cd29a27bfedde048
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-12-02 23:52:50 -0300

    update test hitech logo credits with loop

--- src/components/Hitech.spec.js
+++ src/components/Hitech.spec.js
@@ -4,8 +4,34 @@ import Hitech from './Hitech.vue'
 
 const wrapper = shallowMount(Hitech)
 
+const credits_logo = [
+  { name: 'Javascript'      },
+  { name: 'HTML5',          },
+  { name: 'CSS3'            },
+  { name: 'Vue.js'          },
+  { name: 'Element'         },
+  { name: 'Firebase'        },
+  { name: 'Pug.js'          },
+  { name: 'Stylus.js'       },
+  { name: 'Github'          },
+  { name: 'Git'             },
+  { name: 'Google'          },
+  { name: 'Heroku'          },
+  { name: 'Babel.js'        },
+  { name: 'Node.js'         },
+  { name: 'Cypress'         },
+  { name: 'Firefox'         },
+  { name: 'view.about.work' }
+], credits_logo_array = wrapper.findAll('.hitech a.credits_logo p')
+
 describe('Hitech', () => {
   it('is Hitech a vue instance', () => {
     expect(wrapper.exists()).toBeTruthy()
+  })
+
+  credits_logo_array.wrappers.forEach((wrapper, index) => {
+    it(`Hitech text logo ${credits_logo[index].name}`, () => {
+      expect(wrapper.text()).toContain(credits_logo[index].name)
+    })
   })
 })
