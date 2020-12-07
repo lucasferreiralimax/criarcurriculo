bbe6092ae2928ab42c938fb84c0573f58f4bc4c4
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-12-07 03:38:33 -0300

    test:e2e adjusment credits with list tech and list languages

--- tests/e2e/specs/ValidarCredits.js
+++ tests/e2e/specs/ValidarCredits.js
@@ -1,80 +1,28 @@
-function testeMakeHeart(name, lang) {
+import { list_languages } from '../../../src/i18n/list_languages'
+
+const list_tech = ['Github', 'Heroku', 'Google', 'Firebase', 'HTML5', 'CSS3', 'Javascript', 'Pug.js', 'Stylus.js', 'Babel.js', 'Node.js', 'Git', 'Vue.js', 'Cypress', 'Firefox', 'Element']
+
+function testeMakeHeart(lang, text) {
   cy.wait(500)
   cy.get('#locale').select(lang)
-  cy.contains('.credits_logo p', name)
+  cy.contains('.credits_logo p', text)
 }
 
 describe('Validar Tecnologias', () => {
-  it('Github', () => {
+  it('About page visit scroll .hitech', () => {
     cy.visit('/about')
     cy.get('.hitech').scrollIntoView()
-    cy.contains('.credits_logo p', 'Github')
   })
-  it('Heroku', () => {
-    cy.contains('.credits_logo p', 'Heroku')
-  })
-  it('Google', () => {
-    cy.contains('.credits_logo p', 'Google')
-  })
-  it('Firebase', () => {
-    cy.contains('.credits_logo p', 'Firebase')
-  })
-  it('HTML5', () => {
-    cy.contains('.credits_logo p', 'HTML5')
-  })
-  it('CSS3', () => {
-    cy.contains('.credits_logo p', 'CSS3')
-  })
-  it('Javascript', () => {
-    cy.contains('.credits_logo p', 'Javascript')
-  })
-  it('Pug.js', () => {
-    cy.contains('.credits_logo p', 'Pug.js')
-  })
-  it('Stylus.js', () => {
-    cy.contains('.credits_logo p', 'Stylus.js')
-  })
-  it('Babel.js', () => {
-    cy.contains('.credits_logo p', 'Babel.js')
-  })
-  it('Node.js', () => {
-    cy.contains('.credits_logo p', 'Node.js')
-  })
-  it('Git', () => {
-    cy.contains('.credits_logo p', 'Git')
-  })
-  it('Vue.js', () => {
-    cy.contains('.credits_logo p', 'Vue.js')
-  })
-  it('Cypress', () => {
-    cy.contains('.credits_logo p', 'Cypress')
-  })
-  it('Firefox', () => {
-    cy.contains('.credits_logo p', 'Firefox')
-  })
-  it('Element', () => {
-    cy.contains('.credits_logo p', 'Element')
-  })
+  for(let tech of list_tech) {
+    it(`Tech ${tech}`, () => {
+      cy.contains('.credits_logo p', tech)   
+    })
+  }
   it('Feito com Coração', () => {
-    testeMakeHeart('Hecho con Corazón', 'es-ES')
-    testeMakeHeart('Fet amb Cor', 'es-CA')
-    testeMakeHeart('Made with Heart', 'en-US')
-    testeMakeHeart('Сделано с сердцем', 'ru-RU')
-    testeMakeHeart('ハートで作られた', 'ja-JP')
-    testeMakeHeart('Fait avec du coeur', 'fr-FR')
-    testeMakeHeart('Kalp ile Yapıldı', 'tr-TR')
-    testeMakeHeart('用心做', 'ch-ZH')
-    testeMakeHeart('Mit Herz gemacht', 'al-DE')
-    testeMakeHeart('Fatto con il cuore', 'it-IT')
-    testeMakeHeart('Made with Heart', 'gr-GK')
-    testeMakeHeart('ทำด้วยหัวใจ', 'tl-TD')
-    testeMakeHeart('Gemaakt met hart', 'nl-HL')
-    testeMakeHeart('Made with Heart', 'nl-NL')
-    testeMakeHeart('مصنوعة من القلب', 'ar-SA')
-    testeMakeHeart('दिल से बनाया गय', 'in-ID')
-    testeMakeHeart('Làm bằng trái tim', 'vn-VT')
-    testeMakeHeart('Szívvel készült', 'hu-RV')
-    testeMakeHeart('Déanta le Croí', 'ir-IS')
-    testeMakeHeart('Feito com Coração', 'pt-BR')
+    for(let language in list_languages) {
+      it(`Language ${list_languages[language][0]}`, () => {
+        testeMakeHeart(list_languages[language][1], list_languages[language][3])
+      })
+    }
   })
 })
