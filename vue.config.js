2d244e79e7400622bd8c2770968a7b28083571cb
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-12-20 08:12:20 -0800

    prefetch for preload at audit google

--- vue.config.js
+++ vue.config.js
@@ -15,5 +15,20 @@ module.exports = {
       }
     }
   },
+  chainWebpack: (config) => {
+    /* 
+       Disable (or customize) prefetch, see:
+       https://cli.vuejs.org/guide/html-and-static-assets.html#prefetch
+    */
+    config.plugins.delete('prefetch')
+    /* 
+       Configure preload to load all chunks
+       NOTE: use `allChunks` instead of `all` (deprecated)
+    */
+    config.plugin('preload').tap((options) => {
+      options[0].include = 'allChunks'
+      return options
+    })
+  },
   productionSourceMap: false
 }
