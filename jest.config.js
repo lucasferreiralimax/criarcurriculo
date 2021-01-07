ed28bbef39820520b41c126814fc4e432f8029c3
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-07 19:30:42 -0300

    jest config with transform svg

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   transform: {
     '^.+\\.vue$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '.+\\.(svg|css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.jsx?$': 'babel-jest'
   },
   moduleNameMapper: {
