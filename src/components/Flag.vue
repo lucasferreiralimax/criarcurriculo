f7af6d94d421c80b85195a0bf5e9ef74b0b72c1e
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-07 19:34:33 -0300

    refactory Flag component with list_language loop dynamic img

--- src/components/Flag.vue
+++ src/components/Flag.vue
@@ -1,31 +1,20 @@
 <template lang="pug">
 .flag
-  img(v-if="type == 'pt-BR'" src="./../assets/flags/br.svg" alt="Brasil" width="30")
-  img(v-if="type == 'ar-SA'" src="./../assets/flags/sa.svg" alt="Arábia" width="30")
-  img(v-if="type == 'en-US'" src="./../assets/flags/us.svg" alt="United States" width="30")
-  img(v-if="type == 'es-ES'" src="./../assets/flags/es.svg" alt="Spain" width="30")
-  img(v-if="type == 'es-CA'" src="./../assets/flags/ca.svg" alt="Català" width="30")
-  img(v-if="type == 'ru-RU'" src="./../assets/flags/ru.svg" alt="Russia" width="30")
-  img(v-if="type == 'in-ID'" src="./../assets/flags/in.svg" alt="India" width="30")
-  img(v-if="type == 'ja-JP'" src="./../assets/flags/jp.svg" alt="Japan" width="30")
-  img(v-if="type == 'fr-FR'" src="./../assets/flags/fr.svg" alt="França" width="30")
-  img(v-if="type == 'tr-TR'" src="./../assets/flags/tr.svg" alt="Turquia" width="30")
-  img(v-if="type == 'ch-ZH'" src="./../assets/flags/ch.svg" alt="China" width="30")
-  img(v-if="type == 'al-DE'" src="./../assets/flags/al.svg" alt="Alemanha" width="30")
-  img(v-if="type == 'it-IT'" src="./../assets/flags/it.svg" alt="Italia" width="30")
-  img(v-if="type == 'gr-GK'" src="./../assets/flags/gr.svg" alt="Grecia" width="30")
-  img(v-if="type == 'vn-VT'" src="./../assets/flags/vn.svg" alt="Vietnã" width="30")
-  img(v-if="type == 'tl-TD'" src="./../assets/flags/tl.svg" alt="Tailand" width="30")
-  img(v-if="type == 'nl-HL'" src="./../assets/flags/nh.svg" alt="Holandes" width="30")
-  img(v-if="type == 'nl-NL'" src="./../assets/flags/nl.svg" alt="Noruegues" width="30")
-  img(v-if="type == 'hu-RV'" src="./../assets/flags/hu.svg" alt="Ungáiris" width="30")
-  img(v-if="type == 'ir-IS'" src="./../assets/flags/ir.svg" alt="Ireland" width="30")
+  template(v-for="flag in languages")
+    img(v-if="type == flag[1]" :src="require(`@/assets/flags/${flag[1]}.svg`)" :alt="flag[0]" width="30")
 </template>
 
 <script>
+import { list_languages } from '../i18n/list_languages'
+
 export default {
   name: 'flag',
-  props: ['type']
+  props: ['type'],
+  data () {
+    return {
+      languages: list_languages
+    }
+  }
 }
 </script>
 
