1aa73f3495bf7f6044288d78d83252d1d5bfb94f
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-07 19:33:06 -0300

    test:unit update Flag with list_languages loop

--- src/components/Flag.spec.js
+++ src/components/Flag.spec.js
@@ -4,37 +4,19 @@ import Flag from './Flag.vue'
 
 const wrapper = shallowMount(Flag)
 
-let flags = [
-  { name: 'Brasil',        type: 'pt-BR' },
-  { name: 'United States', type: 'en-US' },
-  { name: 'Spain',         type: 'es-ES' },
-  { name: 'Arábia',        type: 'ar-SA' },
-  { name: 'Català',        type: 'es-CA' },
-  { name: 'Russia',        type: 'ru-RU' },
-  { name: 'India',         type: 'in-ID' },
-  { name: 'Japan',         type: 'ja-JP' },
-  { name: 'França',        type: 'fr-FR' },
-  { name: 'Turquia',       type: 'tr-TR' },
-  { name: 'China',         type: 'ch-ZH' },
-  { name: 'Alemanha',      type: 'al-DE' },
-  { name: 'Grecia',        type: 'gr-GK' },
-  { name: 'Vietnã',        type: 'vn-VT' },
-  { name: 'Tailand',       type: 'tl-TD' },
-  { name: 'Holandes',      type: 'nl-HL' },
-  { name: 'Noruegues',     type: 'nl-NL' },
-  { name: 'Ungáiris',      type: 'hu-RV' },
-  { name: 'Ireland',       type: 'ir-IS' }
-]
+import { list_languages } from '../i18n/list_languages'
+
+let flags = list_languages
 
 describe('Flag', () => {
   it('is Flag a vue instance', () => {
     expect(wrapper.exists()).toBeTruthy()
   })
-  for(let flag of flags) {
-    it(`Flag ${flag.name}`, async () => {
-      await wrapper.setProps({ type: flag.type })
-      expect(wrapper.vm.type).toContain(flag.type)
-      expect(wrapper.find(`.flag img[alt="${flag.name}"]`).exists()).toBeTruthy()
+  for(let flag in flags) {
+    it(`Flag ${list_languages[flag][0]}`, async () => {
+      await wrapper.setProps({ type: list_languages[flag][1] })
+      expect(wrapper.vm.type).toContain(list_languages[flag][1])
+      expect(wrapper.find(`.flag img[alt="${list_languages[flag][0]}"]`).exists()).toBeTruthy()
     })
   }
 })
