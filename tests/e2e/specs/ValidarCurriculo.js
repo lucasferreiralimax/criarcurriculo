53a90b7a4a21823110304a5aecd00112d2a86269
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-07 23:24:30 -0300

    adjustmen tests with clean before type value

--- tests/e2e/specs/ValidarCurriculo.js
+++ tests/e2e/specs/ValidarCurriculo.js
@@ -12,15 +12,15 @@ describe('Validar curriculo simples', () => {
     cy.get('[for="GET-maritalstatus-s"]').click()
   })
   it('Idade', () => {
-    cy.get('#GET-dataofbirth').type(23)
+    cy.get('#GET-dataofbirth').clear().type(26)
   })
   it('Hobbies', () => {
     cy.get('.select-hobby .btn.plus.bullet.small').click()
-    cy.get('.select-hobby input').type('teste')
+    cy.get('.select-hobby input').clear().type('teste')
   })
   it('CEP', () => {
-    cy.get('#GET-cep').type('03816130')
-    cy.get('#GET-address-number').type('1')
+    cy.get('#GET-cep').clear().type('03816130')
+    cy.get('#GET-address-number').clear().type('1')
   })
   it('Disponivel para viagens', () => {
     cy.get('#GET-travel-no').click()
@@ -28,11 +28,10 @@ describe('Validar curriculo simples', () => {
   })
   it('Telefone', () => {
     cy.get('#newTelephone').click()
-    cy.get('#GET-telephone-0').type(11951401894)
+    cy.get('#GET-telephone-0').clear().type(11951401894)
   })
   it('E-mail', () => {
-    cy.get('#newEmail').click()
-    cy.get('#GET-email-0').type('lucasferreiralimax@gmail.com')
+    cy.get('#GET-email-0').clear().type('lucasferreiralimax@gmail.com')
   })
   it('Idiomas', () => {
     // clean languages
@@ -40,16 +39,13 @@ describe('Validar curriculo simples', () => {
       cy.wrap($li).clear()
     })
     // new input languages
-    cy.get('#GET-language-0').type('Fluente Português Brasileiro')
-    cy.get('[for=GET-language-0] .btn.plus').click()
-    cy.get('#GET-language-1').type('Speak Basic English')
-    cy.get('[for=GET-language-1] .btn.plus').click()
-    cy.get('#GET-language-2').type('Hablo español básico')
-    cy.get('[for=GET-language-2] .btn.plus').click()
-    cy.get('#GET-language-3').type('Je parle les bases du Français')
+    cy.get('#GET-language-0').clear().type('Fluente Português Brasileiro')
+    cy.get('#GET-language-1').clear().type('Speak Basic English')
+    cy.get('#GET-language-2').clear().type('Hablo español básico')
+    cy.get('#GET-language-3').clear().type('Je parle les bases du Français')
   })
   it('Site', () => {
-    cy.get('#GET-site-0').type('criar-curriculo.web.app')
+    cy.get('#GET-site-0').clear().type('criar-curriculo.web.app')
   })
 })
 
@@ -61,22 +57,22 @@ describe('Validar Formação Acadêmica', () => {
     cy.get('#GET-coursenow-0').click()
   })
   it('Escola', () => {
-    cy.get('#GET-school-0').type('Internet')
+    cy.get('#GET-school-0').clear().type('Internet')
   })
   it('Curso', () => {
-    cy.get('#GET-course-0').type('Front-end')
+    cy.get('#GET-course-0').clear().type('Front-end')
   })
   it('Inicio do curso', () => {
-    cy.get('#GET-coursedata__start-0').type('2018-12{enter}')
+    cy.get('#GET-coursedata__start-0').clear().type('2018-12{enter}')
   })
   it('Término do curso', () => {
-    cy.get('#GET-coursedata__finish-0').type('2018-12{enter}')
+    cy.get('#GET-coursedata__finish-0').clear().type('2018-12{enter}')
   })
   it('Sobre', () => {
-    cy.get('#GET-courseabout-0').type('Desenvolver aplicação e soluções para ajudar os seres vivos.', {force: true})
+    cy.get('#GET-courseabout-0').clear().type('Desenvolver aplicação e soluções para ajudar os seres vivos.', {force: true})
   })
   it('Referência', () => {
-    cy.get('#GET-schoolLink-0').type('https://criar-curriculo.web.app')
+    cy.get('#GET-schoolLink-0').clear().type('https://criar-curriculo.web.app')
   })
 })
 
@@ -88,18 +84,18 @@ describe('Validar Experiências', () => {
     cy.get('#GET-expsnow-0').click()
   })
   it('Empresa', () => {
-    cy.get('#GET-company-0').type('Internet')
+    cy.get('#GET-company-0').clear().type('Internet')
   })
   it('Cargo', () => {
-    cy.get('#GET-exps-0').type('Front-end')
+    cy.get('#GET-exps-0').clear().type('Front-end')
   })
   it('Inicio do trabalho', () => {
-    cy.get('#GET-expsdata__start-0').type('2018-08{enter}')
+    cy.get('#GET-expsdata__start-0').clear().type('2018-08{enter}')
   })
   it('Sobre', () => {
-    cy.get('#GET-expsabout-0').type('Currículo Grátis', {force: true})
+    cy.get('#GET-expsabout-0').clear().type('Criar Currículo', {force: true})
   })
   it('Referência', () => {
-    cy.get('#GET-companyLink-0').type('https://criar-curriculo.web.app')
+    cy.get('#GET-companyLink-0').clear().type('https://criar-curriculo.web.app')
   })
 })
