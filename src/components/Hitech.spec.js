8488f0f33242fce5dbc6f0a29754e0f3c9958783
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-01 20:24:50 -0800

    test: unit validation url in component Hitech list

--- src/components/Hitech.spec.js
+++ src/components/Hitech.spec.js
@@ -4,8 +4,26 @@ import Hitech from './Hitech.vue'
 
 const wrapper = shallowMount(Hitech)
 
-const credits_logo_array = wrapper.findAll('.hitech a.credits_logo p'),
-      credits_logo = [ 'Javascript', 'HTML5', 'CSS3' , 'Vue.js', 'Element', 'Firebase', 'Pug.js', 'Stylus.js', 'Github', 'Git', 'Google', 'Heroku', 'Babel.js', 'Node.js', 'Cypress', 'Firefox', 'view.about.work' ]
+const credits_logo_array = wrapper.findAll('.hitech a.credits_logo')
+const credits_logo = [
+  { name: 'Javascript',      url: 'https://developer.mozilla.org/pt-BR/docs/Aprender/JavaScript' },
+  { name: 'HTML5',           url: 'https://developer.mozilla.org/pt-BR/docs/Web/HTML/HTML5' },
+  { name: 'CSS3',            url: 'https://developer.mozilla.org/pt-BR/docs/Web/CSS/CSS3' },
+  { name: 'Vue.js',          url: 'https://vuejs.org' },
+  { name: 'Element',         url: 'https://element.eleme.io' },
+  { name: 'Firebase',        url: 'https://firebase.google.com' },
+  { name: 'Pug.js',          url: 'https://pugjs.org' },
+  { name: 'Stylus.js',       url: 'http://stylus-lang.com' },
+  { name: 'Github',          url: 'https://github.com/lucasferreiralimax/criarcurriculo' },
+  { name: 'Git',             url: 'https://git-scm.com' },
+  { name: 'Google',          url: 'https://google.com' },
+  { name: 'Heroku',          url: 'https://www.heroku.com' },
+  { name: 'Babel.js',        url: 'https://babeljs.io' },
+  { name: 'Node.js',         url: 'https://nodejs.org' },
+  { name: 'Cypress',         url: 'https://www.cypress.io' },
+  { name: 'Firefox',         url: 'https://www.mozilla.org' },
+  { name: 'view.about.work', url: 'https://github.com/lucasferreiralimax/criarcurriculo' }
+]
 
 describe('Hitech', () => {
   it('is Hitech a vue instance', () => {
@@ -13,8 +31,11 @@ describe('Hitech', () => {
   })
 
   credits_logo_array.wrappers.forEach((wrapper, index) => {
-    it(`Hitech text logo ${credits_logo[index]}`, () => {
-      expect(wrapper.text()).toContain(credits_logo[index])
+    it(`Hitech text logo ${credits_logo[index].name}`, () => {
+      expect(wrapper.find('p').text()).toContain(credits_logo[index].name)
+    })
+    it(`Hitech url ${credits_logo[index]}`, () => {
+      expect(wrapper.attributes('href')).toBe(credits_logo[index].url)
     })
   })
 })
--- src/components/Hitech.vue
+++ src/components/Hitech.vue
@@ -59,7 +59,7 @@ article.hitech
         path(d="M254.354 282.16L221.402 79.218c-1.03-6.35-7.558-8.977-12.103-4.424L1.29 282.6l114.339 63.908a23.943 23.943 0 0 0 23.334.006l115.392-64.355z" fill="#FCCA3F")
         path(d="M139.12 345.64a24.126 24.126 0 0 1-23.512-.005L.931 282.015l-.93.983 115.607 64.548a24.126 24.126 0 0 0 23.513.005l116.275-64.847-.285-1.752-115.99 64.689z" fill="#EEAB37")
       p Firebase
-    a.credits_logo(href="https://pugjs.org/api/getting-started.html" target="_blank" rel="noopener")
+    a.credits_logo(href="https://pugjs.org" target="_blank" rel="noopener")
       img(src="./../assets/pug.svg" alt="Logo Pug" width="55")
       p Pug.js
     a.credits_logo(href="http://stylus-lang.com" target="_blank" rel="noopener")
@@ -111,7 +111,7 @@ article.hitech
         path(d="M128 7.386c66.667 0 120.614 53.947 120.614 120.614S194.667 248.614 128 248.614 7.386 194.667 7.386 128 61.333 7.386 128 7.386z" fill="#5C5C5E" mask="url(#b)")
         path(d="M192.035 181.07c-4.386 13.597-10.965 23.684-20.175 31.14-9.21 7.457-21.492 11.404-36.842 12.72l-3.07-20.176c10.087-1.315 17.543-3.508 22.368-7.017 1.754-1.316 5.263-5.263 5.263-5.263L123.175 75.807h30.264l21.052 87.28 22.369-87.28h29.386l-34.21 105.263zM86.333 71.86c7.018 0 13.597.877 18.86 3.07 5.702 2.193 10.965 5.263 16.228 9.649l-12.28 16.667c-3.51-2.632-7.018-4.386-10.088-5.702-3.07-1.316-7.018-1.755-10.527-1.755-14.912 0-22.368 11.404-22.368 34.65 0 11.842 1.754 20.175 5.702 25 3.947 5.263 9.21 7.456 16.666 7.456 3.51 0 7.018-.439 10.088-1.755 3.07-1.315 6.579-3.07 10.965-5.701l12.28 17.543c-10.087 8.334-21.49 12.281-34.648 12.281-10.527 0-19.299-2.193-27.193-6.579-7.457-4.386-13.597-10.965-17.544-19.298-3.948-8.333-6.14-17.982-6.14-29.386 0-10.965 2.192-21.053 6.14-29.386 3.947-8.772 10.087-15.35 17.544-20.175 7.456-3.948 16.228-6.58 26.315-6.58z" fill="#FFF" mask="url(#b)")
       p Cypress
-    a.credits_logo(href="https://www.cypress.io" target="_blank" rel="noopener")
+    a.credits_logo(href="https://www.mozilla.org" target="_blank" rel="noopener")
       img(src="./../assets/firefox.svg" alt="Logo Firefox" width="50")
       p Firefox
     a.credits_logo(href="https://github.com/lucasferreiralimax/criarcurriculo" target="_blank" rel="noopener")
