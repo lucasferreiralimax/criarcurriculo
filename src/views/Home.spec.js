22e718b5f05a5c3fd918585d11bea211de0af937
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-05-21 06:26:37 -0700

    test(unit): update spec Home page

--- src/views/Home.spec.js
+++ src/views/Home.spec.js
@@ -3,7 +3,6 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import { mutations } from '@/mutations'
 import Vuex from 'vuex'
 import Home from './Home.vue'
-import { Hooper, Slide, Pagination as HooperPagination, Navigation as HooperNavigation } from 'hooper';
 
 const localVue = createLocalVue()
 
@@ -23,17 +22,7 @@ describe('Home', () => {
   it('is Home a vue instance', () => {
     expect(wrapper.exists()).toBeTruthy()
   })
-  it('is Home with Carousel component', () => {
-    expect(wrapper.findComponent(Hooper).exists()).toBeTruthy()
-  })
-  it('is Home with Carousel component item', () => {
-    expect(wrapper.findComponent(Slide).exists()).toBeTruthy()
-  })
-  it('is Home with Carousel component Pagination', () => {
-    expect(wrapper.findComponent(HooperPagination).exists()).toBeTruthy()
-  })
   it('is Home with iframe facebook', () => {
     expect(wrapper.findAll('iframe.facebook').at(0).exists()).toBeTruthy()
-    expect(wrapper.findAll('iframe.facebook').at(1).exists()).toBeTruthy()
   })
 })
