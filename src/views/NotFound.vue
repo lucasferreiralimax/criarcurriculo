db289ebb3664d7a3c21e38c654842d893da79c09
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-05-21 05:40:08 -0700

    migrating router-link tag

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -3,7 +3,7 @@ section.mark.text-center
   h1 {{ $t('view.notfound.text1') }}
   p {{ $t('view.notfound.text2') }}
   h3 {{ $t('view.notfound.text3') }}
-  router-link(to="/" tag="a") {{ $t('view.notfound.click') }}
+  router-link(to="/") {{ $t('view.notfound.click') }}
 </template>
 
 <script>
