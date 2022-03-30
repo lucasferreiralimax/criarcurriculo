c221efd524c7ed5269e9b29a9cd4152658fd84c1
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-03-30 20:51:15 -0300

    delete old files version 2

--- version.2/src/api.js
+++
@@ -1,5 +0,0 @@
-export const api = {
-  'firebase': 'https://curriculo-gratis.firebaseio.com',
-  'viacep': 'https://viacep.com.br/ws/',
-  'github': 'https://api.github.com/repos/lucasferreiralimax/criarcurriculo/contributors'
-}
--- version.2/src/directive/resize.js
+++
@@ -1,9 +0,0 @@
-export const resizeTextArea = {
-  bind (el) { el.addEventListener("click", () => resize(el)) },
-  componentUpdated(el) { resize(el) }
-}
-
-function resize(el) {
-  el.style.cssText = 'height:auto; padding:0'
-  el.style.cssText = 'height:' + (el.scrollHeight < 50 ? 50 : el.scrollHeight) + 'px'
-}
