a28fa7a1a9742b6c8548705651e50d6245e3161c
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-06-16 19:54:29 -0300

    add clearable in inputs

--- src/components/Curriculo.vue
+++ src/components/Curriculo.vue
@@ -36,50 +36,50 @@ Box.personal
   template(#heading) Dados pessoais
   v-row
     v-col(cols="12" sm="6")
-      v-text-field(label="Nome completo" v-model="curriculo.name" hide-details="auto")
+      v-text-field(label="Nome completo" v-model="curriculo.name" hide-details="auto" clearable)
     v-col(cols="12" sm="6" v-if="curriculo.name || curriculo.age")
-      v-text-field(label="Idade" v-model="curriculo.age" type="number" hide-details="auto")
+      v-text-field(label="Idade" v-model="curriculo.age" type="number" hide-details="auto" clearable)
   v-row
     v-col(cols="12" sm="6" v-if="(curriculo.name && curriculo.age) || curriculo.gender")
-      v-combobox(v-model="curriculo.gender" :items="genders" label="Genero" outlined dense hide-details="auto")
+      v-combobox(v-model="curriculo.gender" :items="genders" label="Genero" outlined dense hide-details="auto" clearable)
     v-col(cols="12" sm="6" v-if="(curriculo.name && curriculo.age && curriculo.gender) || curriculo.maritial")
-      v-combobox(v-model="curriculo.maritial" :items="maritials" label="Estado civil" outlined dense hide-details="auto")
+      v-combobox(v-model="curriculo.maritial" :items="maritials" label="Estado civil" outlined dense hide-details="auto" clearable)
 Box.address
   template(#icon)
     HomeIcon
   template(#heading) Endereço
   v-row
     v-col(cols="12" sm="6")
-      v-text-field(@keyup="search_cep" label="CEP" v-model="curriculo.address.cep" type="tel" hide-details="auto" maxlength="9")
+      v-text-field(@keyup="search_cep" label="CEP" v-model="curriculo.address.cep" type="tel" hide-details="auto" maxlength="9" clearable)
     v-col(cols="12" sm="6" v-if="curriculo.address.cep || curriculo.address.number")
-      v-text-field(label="Número" v-model="curriculo.address.number" type="number" hide-details="auto")
+      v-text-field(label="Número" v-model="curriculo.address.number" type="number" hide-details="auto" clearable)
   v-row
     v-col(cols="12" sm="6" v-if="(curriculo.address.cep && curriculo.address.number) || curriculo.address.city")
-      v-text-field(label="Cidade" v-model="curriculo.address.city" type="text" hide-details="auto")
+      v-text-field(label="Cidade" v-model="curriculo.address.city" type="text" hide-details="auto" clearable)
     v-col(cols="12" sm="6" v-if="(curriculo.address.cep && curriculo.address.number && curriculo.address.city) || curriculo.address.country")
-      v-text-field(label="Estado" v-model="curriculo.address.country" type="text" hide-details="auto")
+      v-text-field(label="Estado" v-model="curriculo.address.country" type="text" hide-details="auto" clearable)
   v-row
     v-col(cols="12" sm="6" v-if="(curriculo.address.cep && curriculo.address.number && curriculo.address.city && curriculo.address.country) || curriculo.address.street")
-      v-text-field(label="Local" v-model="curriculo.address.street" type="text" hide-details="auto")
+      v-text-field(label="Local" v-model="curriculo.address.street" type="text" hide-details="auto" clearable)
 Box.languages
   template(#icon)
     DocumentationIcon
   template(#heading) Idiomas
   v-row
     v-col(cols="12")
-      v-combobox(v-model="curriculo.languages" :items="languages" label="Idioma" outlined dense hide-details="auto" multiple)
+      v-combobox(label="Languages" v-model="curriculo.languages" :items="languages" outlined dense :label="false" hide-details="auto" multiple chips clearable)
 Box.academy
   template(#icon)
     DocumentationIcon
   template(#heading) Formação
   v-row
     v-col(cols="12" sm="6")
-      v-text-field(label="Nome completo" v-model="curriculo.name" hide-details="auto")
+      v-text-field(label="Nome completo" v-model="curriculo.name" hide-details="auto" clearable)
 Box.experience
   template(#icon)
     DocumentationIcon
   template(#heading) Experiência
   v-row
     v-col(cols="12" sm="6")
-      v-text-field(label="Nome completo" v-model="curriculo.name" hide-details="auto")
+      v-text-field(label="Nome completo" v-model="curriculo.name" hide-details="auto" clearable)
 </template>
