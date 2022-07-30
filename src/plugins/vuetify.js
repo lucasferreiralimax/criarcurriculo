b5925578f64624587ebdf6831495fdb8294f95ea
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-07-30 18:42:43 -0300

    update in logic inputs boxs

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,6 +4,18 @@ import "vuetify/styles";
 
 // Vuetify
 import { createVuetify } from "vuetify";
+import colors from 'vuetify/lib/util/colors';
 
-export default createVuetify();
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  theme: {
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: colors.green.darken1,
+          secondary: colors.red.lighten4,
+        }
+      },
+    },
+  },
+});
