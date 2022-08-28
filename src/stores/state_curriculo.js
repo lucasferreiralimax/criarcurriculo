80c40db2c6f6fcfa73a67453158e8780fa7e9328
Lucas <lucasferreiralimax@gmail.com>
2022-08-28 00:21:31 -0300

    Update informativo currículo vivo

--- src/stores/state_curriculo.js
+++ src/stores/state_curriculo.js
@@ -1,7 +1,7 @@
 const stateCurriculo = {
   curriculo: {
     name: "Virgulino Ferreira da Silva",
-    age: 21,
+    age: 40,
     gender: 'Masculino',
     maritial: 'Casado',
     about: 'Vulgo Lampião, o Rei do Cangaço foi um cangaceiro (herói e contraventor) brasileiro que atuou na região do sertão nordestino do Brasil. O apelido "Lampião" foi lhe dado devido ter facilidade em manejar o rifle, "que, de tanto atirar, mais parecia um candeeiro aceso nas escuras noites da caatinga”.',
@@ -13,7 +13,7 @@ const stateCurriculo = {
     },
     formation: [],
     experience: [],
-    languages: [],
+    languages: ['Português brasileiro', 'Espanhol', 'Inglês', 'Russo', 'Francês'],
   }
 };
 
