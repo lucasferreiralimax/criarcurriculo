36e1b72849259b4eecda961754c5f8e5507c1d55
Lucas <lucasferreiralimax@gmail.com>
2022-08-28 00:13:32 -0300

    Adjustment strings

--- src/stores/state_curriculo.js
+++ src/stores/state_curriculo.js
@@ -6,9 +6,9 @@ const stateCurriculo = {
     maritial: 'Casado',
     about: 'Vulgo Lampião, o Rei do Cangaço foi um cangaceiro (herói e contraventor) brasileiro que atuou na região do sertão nordestino do Brasil. O apelido "Lampião" foi lhe dado devido ter facilidade em manejar o rifle, "que, de tanto atirar, mais parecia um candeeiro aceso nas escuras noites da caatinga”.',
     address: {
-      cep: 58840-000,
-      city: Pombal,
-      country: Paraíba,
+      cep: '58840-000',
+      city: 'Pombal',
+      country: 'Paraíba',
       travel: true,
     },
     formation: [],
