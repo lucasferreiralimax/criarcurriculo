763d2dcf26e578378c07fd7f014a17d9ebc2a271
Lucas <lucasferreiralimax@gmail.com>
2022-08-28 17:17:25 -0300

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,8 +5,6 @@ import Logo from "@/components/Logo.vue";
 <template lang="pug">
 footer
   Logo(style="transform: translateX(-30px)")
-    span
-      | 2015 - 2022
 </template>
 
 <style scoped lang="stylus">
@@ -14,9 +12,4 @@ footer
   text-align center
   line-height 1.5
   margin-top 3rem
-  span
-    position absolute
-    top 20px
-    width 80px
-    margin-left 2rem
 </style>
