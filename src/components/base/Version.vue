bbd94f201940f0ebf782a9eeec2ab34902eabe3c
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-02 18:09:11 -0300

    component version app

---
+++ src/components/base/Version.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+small.version {{ $t('version') }} {{ version }}
+</template>
+
+<script>
+import pkg from '../../../package.json'
+
+export default {
+  name: 'version-app',
+  data () {
+    return {
+      version: pkg.version
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.version
+  color color1
+</style>
