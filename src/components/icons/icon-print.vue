b0f37869692d604a7b2804a52d78627056605e35
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-08 19:43:02 -0300

    add more icons in components

---
+++ src/components/icons/icon-print.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+svg.icon-print(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32")
+  path(d="M10 24h10v2H10v-2zM8 2h16v4h2V2c0-1.103-.896-2-2-2H8C6.896 0 6 .896 6 2v4h2V2zm2 18h12v2H10v-2z")
+  path(d="M30 8H2c-1.104 0-2 .896-2 2v12c0 1.105.896 2 2 2h4v6c0 1.105.896 2 2 2h16c1.104 0 2-.895 2-2v-6h4c1.104 0 2-.895 2-2V10c0-1.104-.897-2-2-2zm-6 22H8V18h16v12zm4-16c-1.104 0-2-.895-2-2 0-1.104.896-2 2-2s2 .896 2 2c0 1.105-.897 2-2 2z")
+</template>
+
+<script>
+export default { name: 'icon-print' }
+</script>
+
+
--- src/components/lab/app-experiencies/index.vue
+++ src/components/lab/app-experiencies/index.vue
@@ -22,8 +22,7 @@ draggable(:list="user.exps"
         button.handle.btn.bullet.small.left(type="button" :aria-label="$t('aria-label.move') + ' ' + exp.experience.name")
           i =
         button.btn.delete.bullet.small.left(type="button" @click="remove('exps', key)" :aria-label="$t('aria-label.exp_remove') + ' ' + exp.experience.name")
-          svg(viewBox="0 0 329.269 329" xmlns="http://www.w3.org/2000/svg")
-            path(d="M194.8 164.77L323.013 36.555c8.343-8.34 8.343-21.825 0-30.164-8.34-8.34-21.825-8.34-30.164 0L164.633 134.605 36.422 6.391c-8.344-8.34-21.824-8.34-30.164 0-8.344 8.34-8.344 21.824 0 30.164l128.21 128.215L6.259 292.984c-8.344 8.34-8.344 21.825 0 30.164a21.266 21.266 0 0015.082 6.25c5.46 0 10.922-2.09 15.082-6.25l128.21-128.214 128.216 128.214a21.273 21.273 0 0015.082 6.25c5.46 0 10.922-2.09 15.082-6.25 8.343-8.34 8.343-21.824 0-30.164zm0 0")
+          IconDelete
     .row
       label.exps_now(:for="'GET-expsnow-' + key" v-bind:class="{ active: exp.experience.now, 'display-n-print': !exp.experience.now }" @keyup.enter='exp.experience.now = !exp.experience.now')
         input(:id="'GET-expsnow-' + key" type="checkbox" v:name="'now' + key" v-model="exp.experience.now" @click="updateVuex('updateExpCheck', $event, key)" :aria-checked="exp.experience.now")
@@ -44,8 +43,7 @@ draggable(:list="user.exps"
       app-input(:index="key" :name="'companyLink-' + key" :value="{ parent: 'exps', child: 'experience', content: 'link' }" type="text" translate="link" vuex="updateExp")
   app-box
     button.btn.plus.large(v-scroll-to="'#exp-' + user.exps.length" type="button" @click="newComponent('exps')" :aria-label="$t('aria-label.exp_add')")
-      svg.icon(viewBox="0 -31 512 512" xmlns="http://www.w3.org/2000/svg" width="20")
-        path(d="M497.094 60.004c-.031 0-.063-.004-.094-.004H361V45c0-24.813-20.188-45-45-45H196c-24.813 0-45 20.188-45 45v15H15C6.648 60 0 66.844 0 75v330c0 24.813 20.188 45 45 45h422c24.813 0 45-20.188 45-45V75.316v-.058c-.574-9.852-6.633-15.2-14.906-15.254zM181 45c0-8.27 6.73-15 15-15h120c8.27 0 15 6.73 15 15v15H181zm295.188 45l-46.583 139.742A14.975 14.975 0 0 1 415.38 240H331v-15c0-8.285-6.715-15-15-15H196c-8.285 0-15 6.715-15 15v15H96.621a14.975 14.975 0 0 1-14.226-10.258L35.813 90zM301 240v30h-90v-30zm181 165c0 8.27-6.73 15-15 15H45c-8.27 0-15-6.73-15-15V167.434l23.934 71.796A44.935 44.935 0 0 0 96.62 270H181v15c0 8.285 6.715 15 15 15h120c8.285 0 15-6.715 15-15v-15h84.379a44.935 44.935 0 0 0 42.687-30.77L482 167.434zm0 0")
+      IconExperiencies
       span +
       | {{ $t('form.exp') }}
 </template>
@@ -57,12 +55,14 @@ import { scrollTo } from '@/directive/scroll.js'
 import AppBox from '@/components/lab/app-box'
 import AppInput from '@/components/inputs/app-input'
 import AppTextarea from '@/components/inputs/app-textarea'
+import IconDelete from '@/components/icons/icon-delete'
+import IconExperiencies from '@/components/icons/icon-experiencies'
 
 export default {
   name: 'app-experiencies',
   props: ['errors'],
   mixins: [mixinUpdateStore],
-  components: { AppBox, AppInput, AppTextarea },
+  components: { AppBox, AppInput, AppTextarea, IconDelete, IconExperiencies },
   directives: { 'scroll-to': scrollTo },
   data () {
     return {
