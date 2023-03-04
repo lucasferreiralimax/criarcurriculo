990b965c93816c3856666aa389ea2beea3cca378
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-03-04 17:25:31 -0300

    remove ifframe facebook in about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -30,7 +30,6 @@ section.mark.text-center
         | {{ country.name }}
   section.tech
     app-hitech
-    iframe.facebook(src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcurriculonovo&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share")
 </template>
 
 <script>
@@ -60,11 +59,6 @@ export default {
   .tech
     display flex
     text-align left
-
-.facebook
-  margin-top 2rem
-  border-radius 6px
-  min-width 340px
 
 .carousel
   margin 2em auto 0
