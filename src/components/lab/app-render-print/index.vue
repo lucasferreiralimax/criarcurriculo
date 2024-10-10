831f965c58f5465db9743b1ef23e005707f2fec0
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-10-10 02:16:02 -0300

    refactory and use new mixin translateDate

--- src/components/lab/app-render-print/index.vue
+++ src/components/lab/app-render-print/index.vue
@@ -111,64 +111,14 @@ section.render-print(:class="{ 'no-icon' : !icon_render }")
 <script>
 import { mapState } from 'vuex'
 import { mixinUpdateStore } from '@/mixins/mixinUpdateStore.js'
+import { mixinTranslateDate } from '@/mixins/mixinTranslateDate.js'
 
 export default {
   name: 'app-render-print',
   computed: mapState({
-    user: state => state.user,
     icon_render: state => state.icon_render,
     layout_render: state => state.layout_render
   }),
-  mixins: [mixinUpdateStore],
-  methods: {
-    translateDate (date) {
-      if (!date) return ''
-
-      let value = new Date(date),
-          month = value.getMonth(),
-          year = value.getFullYear()
-
-      switch (++month) {
-        case 1:
-          month = this.$t('calendar.january')
-          break
-        case 2:
-          month = this.$t('calendar.february')
-          break
-        case 3:
-          month = this.$t('calendar.march')
-          break
-        case 4:
-          month = this.$t('calendar.april')
-          break
-        case 5:
-          month = this.$t('calendar.may')
-          break
-        case 6:
-          month = this.$t('calendar.june')
-          break
-        case 7:
-          month = this.$t('calendar.july')
-          break
-        case 8:
-          month = this.$t('calendar.august')
-          break
-        case 9:
-          month = this.$t('calendar.september')
-          break
-        case 10:
-          month = this.$t('calendar.october')
-          break
-        case 11:
-          month = this.$t('calendar.november')
-          break
-        case 12:
-          month = this.$t('calendar.december')
-          break
-      }
-
-      return `${month} ${this.$t('calendar.in')} ${year}`
-    }
-  }
+  mixins: [mixinUpdateStore, mixinTranslateDate],
 }
 </script>
--- src/components/lab/app-render/index.vue
+++ src/components/lab/app-render/index.vue
@@ -189,15 +189,15 @@ section.render(:class="{ 'viewfixed' : viewfixed, 'no-icon' : !icon_render }")
 <script>
 import { mapState } from 'vuex'
 import { mixinUpdateStore } from '@/mixins/mixinUpdateStore.js'
+import { mixinTranslateDate } from '@/mixins/mixinTranslateDate.js'
 
 export default {
   name: 'app-render',
   computed: mapState({
-    user: state => state.user,
     icon_render: state => state.icon_render,
     layout_render: state => state.layout_render
   }),
-  mixins: [mixinUpdateStore],
+  mixins: [mixinUpdateStore, mixinTranslateDate],
   created () { window.addEventListener('scroll', this.viewFixed) },
   destroyed () { window.removeEventListener('scroll', this.viewFixed) },
   data () {
@@ -238,54 +238,6 @@ export default {
         this.viewfixed = false
       }
     },
-    translateDate (date) {
-      if (!date) return ''
-
-      let value = new Date(date),
-          month = value.getMonth(),
-          year = value.getFullYear()
-
-      switch (++month) {
-        case 1:
-          month = this.$t('calendar.january')
-          break
-        case 2:
-          month = this.$t('calendar.february')
-          break
-        case 3:
-          month = this.$t('calendar.march')
-          break
-        case 4:
-          month = this.$t('calendar.april')
-          break
-        case 5:
-          month = this.$t('calendar.may')
-          break
-        case 6:
-          month = this.$t('calendar.june')
-          break
-        case 7:
-          month = this.$t('calendar.july')
-          break
-        case 8:
-          month = this.$t('calendar.august')
-          break
-        case 9:
-          month = this.$t('calendar.september')
-          break
-        case 10:
-          month = this.$t('calendar.october')
-          break
-        case 11:
-          month = this.$t('calendar.november')
-          break
-        case 12:
-          month = this.$t('calendar.december')
-          break
-      }
-
-      return `${month} ${this.$t('calendar.in')} ${year}`
-    }
   }
 }
 </script>
